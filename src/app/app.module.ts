b8642bda17684c10da354a863bf38b6d0ce1ff10
christiantomuta <christian.tomuta@gmx.de>
2022-06-09 13:02:39 +0200

    Generated weather app component + user input form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
