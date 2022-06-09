eff1d54e1b34316118dda7f4f388ffaa4196f94a
christiantomuta <christian.tomuta@gmx.de>
2022-06-09 17:35:09 +0200

    Integrated weather API cont'd

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -6,5 +6,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApixuService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getWeather(location: string) {
+    return this.http.get(
+      'http://api.weatherstack.com/current?access_key=7628a753500ccb135c5c0d33c552f02a&query=' + location
+    );
+  }
 }
