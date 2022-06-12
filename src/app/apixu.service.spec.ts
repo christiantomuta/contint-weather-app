219ce626b6dd72afa925461dd01d81505075f069
Jusef N <2770770+jusefn@users.noreply.github.com>
2022-06-12 15:26:23 +0200

    fixed tests for appxuservice

--- src/app/apixu.service.spec.ts
+++ src/app/apixu.service.spec.ts
@@ -2,13 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { ApixuService } from './apixu.service';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
 import {HttpClientModule} from "@angular/common/http";
 import {WeatherComponent} from "./weather/weather.component";
 
 describe('ApixuService', () => {
   let service: ApixuService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(async () => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, FormsModule, HttpClientModule],
       declarations: [ WeatherComponent ],
@@ -18,6 +21,7 @@ describe('ApixuService', () => {
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    service = new ApixuService(httpClientSpy);
+    expect(service).toBeDefined();
   });
 });
