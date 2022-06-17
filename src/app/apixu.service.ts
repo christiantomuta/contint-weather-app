f717822c3485d61012e9e03863b97e13511e0251
Jusef N <2770770+jusefn@users.noreply.github.com>
2022-06-17 15:22:41 +0200

    moved api call to backend

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
   getWeather(location: string) {
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=7628a753500ccb135c5c0d33c552f02a&query=' + location
+      environment.BACKEND_API + "?location=" + location
     );
   }
 }
